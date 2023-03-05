485e568e7c2892c878f76873a32dab7474d112b0
20093293 <20093293@mail.wit.ie>
2023-03-05 14:40:43 +0000

    updated the menu files and added more layout files

--- app/src/main/java/ie/setu/restaurant_review_app/MainActivity.kt
+++ app/src/main/java/ie/setu/restaurant_review_app/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_restaurant)
 
         val addButton = findViewById<Button>(R.id.addButton)
     }

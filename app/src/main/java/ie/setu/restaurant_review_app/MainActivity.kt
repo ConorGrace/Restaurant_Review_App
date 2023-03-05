68005ced85766d255cf7b9bf9a42bacd5d021052
20093293 <20093293@mail.wit.ie>
2023-03-05 14:20:24 +0000

    updated the build gradel with timber etc

--- app/src/main/java/ie/setu/restaurant_review_app/MainActivity.kt
+++ app/src/main/java/ie/setu/restaurant_review_app/MainActivity.kt
@@ -2,10 +2,13 @@ package ie.setu.restaurant_review_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addButton = findViewById<Button>(R.id.addButton)
     }
 }
